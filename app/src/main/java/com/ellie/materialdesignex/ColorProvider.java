f7ca96ca82a5d763bcbfe6e3df6bcddcadb15308
foreknowledge <kyj_0129@naver.com>
2020-08-13 15:06:45 +0900

    feat: 일부 색상 변경

--- app/src/main/java/com/ellie/materialdesignex/ColorProvider.java
+++ app/src/main/java/com/ellie/materialdesignex/ColorProvider.java
@@ -10,7 +10,7 @@ public class ColorProvider {
     private List<String> colorList = Arrays.asList(
             "#E74C3C", "#E67E22", "#F5B041", "#F4D03F", "#2ECC71",
             "#27AE60", "#3498DB", "#2980B9", "#9B59B6", "#8E44AD",
-            "#F7F9F9", "#979A9A", "#5D6D7E", "#34495E", "#283747"
+            "#FFFFFF", "#979A9A", "#5D6D7E", "#34495E", "#283747"
     );
 
     private ArrayList<String> colorArray = new ArrayList<>(colorList);
